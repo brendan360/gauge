9ac914ec3419130489da6573b81675ac8a63c106
brendan360 <brendan360@hotmail.com>
2021-09-29 17:22:40 +1000

    ....

--- gauge.py
+++ gauge.py
@@ -233,7 +233,7 @@ def connectOBD():
 
 def obdTHREAD():
     connection = obd.OBD(obdConnection, check_voltage=False, baudrate=9600)
-    print(conection.query(obd.commands.RPM))
+    print(connection.query(obd.commands.RPM))
  #   for key,value in gaugeItems.items():
  #       if value[1]=="OBD":
  #           value[4]=str(connection.query(obd.commands.RPM))
