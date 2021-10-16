50d07b24e588ef61a1cf49d7edd9a412e5714581
brendan360 <brendan360@hotmail.com>
2021-10-16 17:29:22 +1100

    .....

--- gauge.py
+++ gauge.py
@@ -410,8 +410,7 @@ def alertTHREAD():
             if key == "RPM":
                 if round(int(value[4])) >= 5500 and round(int(value[4])) < 6500:
                     if value[9] == 0:
-                       # value[9]=85500
-                        value[9]=900000
+                        value[9]=85500
                         threading.Thread(target=shiftALERTING).start()
                     else: 
                         value[9]-=1
@@ -423,8 +422,7 @@ def alertTHREAD():
                 if value[9] <= 0:
                     threading.Thread(target=flashLed).start()
                     print("Alert ",key," is going high")
-                    value[9]=9000
-                    time.sleep(2)
+                    value[9]=900000
                     alertScreen=1
                     eval(key +"()")
                 else: 
