772172988f40bd80ca1e593fcce70527b7e926b3
brendan360 <brendan360@hotmail.com>
2021-10-16 14:46:01 +1100

    .

--- gauge.py
+++ gauge.py
@@ -255,7 +255,7 @@ def obdTHREAD():
             if gaugeItems[i][1]=="OBD":
                 cmd= "gaugeItems[i][4]=connection.query(obd.commands."+str(i)+")"
                 exec(cmd)
-                print(str(gaugeItems[i][4]))
+                print(str(gaugeItems[i][4].value))
 
     connection.close()
 
