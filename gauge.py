94986b8066af1428cd1743b19803f3a6775b8ab2
brendan360 <brendan360@hotmail.com>
2021-10-16 16:07:24 +1100

    .

--- gauge.py
+++ gauge.py
@@ -401,7 +401,7 @@ def alertTHREAD():
             if value[8]=="na":
                 continue
 
-            elif int(value[4]) >= int(value[8]):
+            elif int(round(value[4])) >= int(value[8]):
                 if value[9] <= 0:
                     threading.Thread(target=flashLed).start()
                     print("Alert ",key," is going high")
