86ff3686a07a86c1b189e729652270dfa0e77c06
brendan360 <brendan360@hotmail.com>
2021-10-19 13:11:00 +1100

    .

--- gauge.py
+++ gauge.py
@@ -166,7 +166,7 @@ gaugeItems={"ENGINE_LOAD":["04","OBD",0,"Engine Load","0",3,"a","na","100",0],
             "BLOCK_TEMP1_ADC":["ADCPIN2","ADC",0,"Block °C","0",2,"adc","na","90",0],
             "BLOCK_TEMP2_ADC":["ADCPIN3","ADC",0,"Head °C","0",3,"adc","na","90",0],
             "CABIN_TEMP_i2c":["TEMPADDR","I2C",1,"Cabin °C","0",4,"adc","na","na",0],
-            "ALTITUDE_i2c":["ALTADDR","I2C",1,"Altitude","0",4,"adc","na","na",0]
+            "ALTITUDE_i2c":["ALTADDR","I2C",1,"Altitude","0",4,"adc","na","na",0],
             "CPU_temp":["ALTADDR","I2C",1,"CPU °C","0",4,"adc","na","na",0]
             }
 
